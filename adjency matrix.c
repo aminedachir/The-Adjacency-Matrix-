1b499058f6ac905ba5aaa8f98cc3db1bf8051e6f
aminedachir <dachiramine110@gmail.com>
2024-11-09 15:40:28 +0100

    The incidence matrix

--- adjency matrix.c
+++ adjency matrix.c
@@ -5,7 +5,7 @@ int main(){
     int arr[n][n];
         for (int i = 1;i<=n;i++){
             for (int j = 1;j<=n;j++){
-                printf("\nsi{%d, %d} in E ecrire 1 sinon 0",i,j);
+                printf("\nsi{%d, %d} in E ecrire 1 sinon 0 : ",i,j);
                 scanf("%d",&arr[i][j]);
         }
     }
