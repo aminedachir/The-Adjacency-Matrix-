af9780243c83bf6a319b033f262d54e76a0903af
aminedachir <dachiramine110@gmail.com>
2024-11-09 15:24:43 +0100

    update

--- adjency matrix.c
+++ adjency matrix.c
@@ -2,24 +2,18 @@
 int main(){
     int n;
     scanf("%d",&n);
-    n = n+1;
     int arr[n][n];
-        for (int i = 1;i<n;i++){
-            for (int j = 1;j<n;j++){
+        for (int i = 1;i<=n;i++){
+            for (int j = 1;j<=n;j++){
                 printf("\nsi{%d, %d} in E ecrire 1 sinon 0",i,j);
                 scanf("%d",&arr[i][j]);
         }
     }
-    printf("The Adjacency Matrix :")
-    for (int i = 1;i<n;i++){
-            for (int j = 1;j<n;j++){
+    printf("The Adjacency Matrix :\n");
+    for (int i = 1;i<=n;i++){
+            for (int j = 1;j<=n;j++){
                 printf("   %d",arr[i][j]);
         }
         printf("\n");
     }
-<<<<<<< HEAD
-
-    
-=======
->>>>>>> 474c149f9ae33b6eda2b39fb539fa270a71bca49
 }
