effae7ff91d87e09a6e6175eb077dfb2c6b7b6e9
aminedachir <dachiramine110@gmail.com>
2024-11-20 23:41:15 +0100

    add pointers

--- cycle_and_complete_graphs.c
+++ cycle_and_complete_graphs.c
@@ -1,19 +1,15 @@
 #include <stdio.h>
 #include <stdbool.h>
 int n,m;
-//int *ptr;
-//ptr = &arr[n][n];
-void matrix(int n,int m){
-    int arr[n][n];
+void matrix(int n,int m,int **arr){
     for (int i=0;i<n;i++){
-        printf("Give me the first line in your matrix : ");
+        printf("Give me the %d line in your matrix : ",i+1);
         for(int j=0;j<m;j++){
             scanf("%d",&arr[i][j]);
         }
     }
 }
-void print_matrix(int n,int m){
-    int arr[n][n];
+void print_matrix(int n,int m,int **arr){
     for (int i = 0;i<n;i++){
             for (int j = 0;j<m;j++){
                 printf("   %d",arr[i][j]);
@@ -21,10 +17,9 @@ void print_matrix(int n,int m){
         printf("\n");
     }
 }
-bool cycle_graph(int n){
-    int arr[n][n];
+bool cycle_graph(int n,int m,int **arr){
     int s=0;
-    matrix(n,m);
+    //matrix(n,m,arr);
     for (int i=0;i<n;i++){
         for(int j=0;j<n;j++){
             if (arr[i][j] == 1){
@@ -39,21 +34,19 @@ bool cycle_graph(int n){
         return false;
     }
 }
-
-bool compeleted_graph(int n){
-    int arr[n][n];
-    int m=0;
+bool compeleted_graph(int n,int m,int **arr){
+    int d=0;
     for (int i=0;i<n;i++){
         int s=0;
         for(int j=0;j<n;j++){
-                printf("\nsi{Vertice %d, Vertice %d} in E ecrire 1 sinon 0 : ",i+1,j+1);
-                scanf("%d",&arr[i][j]);
+                //printf("\nsi{Vertice %d, Vertice %d} in E ecrire 1 sinon 0 : ",i+1,j+1);
+                //scanf("%d",&arr[i][j]);
                 if (arr[i][j] == 1){
                     s++;
                 }
         }
         if (s == n-1){
-            m++;
+            d++;
         }
     }
     if (m == n){
--- main.c
+++ main.c
@@ -5,21 +5,27 @@
 #include "cycle_and_complete_graphs.c"
 int main(){
     int n,m;
-    scanf("%d    %d",&n,&m);
-    int arr[n][n];
-    matrix(n,m);
-    print_matrix(n,m);
-    if (cycle_graph(n) == true){
-        printf("Its a cycle graph");
+    printf("Enter the number of vertices (n): ");
+    scanf("%d",&n);
+    printf("Enter the number of edges (m): ");
+    scanf("%d", &m);
+    int** arr = (int**)malloc(n * sizeof(int*));
+    for (int i = 0; i < n; i++) {
+        arr[i] = (int*)malloc(m * sizeof(int));
+    }
+    matrix(n,m,arr);
+    print_matrix(n,m,arr);
+    if (cycle_graph(n,m,arr) == true){
+        printf("Its a cycle graph\n");
     }
     else{
-        printf("Its Not a cycle graph");
+        printf("Its Not a cycle graph\n");
     }
-    if (compeleted_graph(n) == true){
-        printf("Its a compeleted graph");
+    if (compeleted_graph(n,m,arr) == true){
+        printf("Its a compeleted graph\n");
     }
     else{
-        printf("Its Not a completed graph");
+        printf("Its Not a completed graph\n");
     }
     return 0;
 }
