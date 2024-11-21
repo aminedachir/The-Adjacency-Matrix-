6502126ec45997f9c092b4d4fd2626fac7f7d4fb
aminedachir <dachiramine110@gmail.com>
2024-11-21 23:49:24 +0100

    scan input by line and modified the functions names

--- functions.c
+++ functions.c
@@ -1,25 +1,24 @@
 #include <stdio.h>
 #include <stdbool.h>
-int n,m;
-void matrix(int n,int m,int **arr){
+int n,m,x;
+void registre_graph(int n,int m,int **arr){
     for (int i=0;i<n;i++){
-        printf("Give me the %d line in your matrix : ",i+1);
+        //printf("Give me the %d line in your matrix : ",i+1);
         for(int j=0;j<m;j++){
-            scanf("%d",&arr[i][j]);
+            scanf("%d ",&arr[i][j]);
         }
     }
 }
-void print_matrix(int n,int m,int **arr){
+void print_graph(int n,int m,int **arr){
     for (int i = 0;i<n;i++){
             for (int j = 0;j<m;j++){
-                printf("   %d",arr[i][j]);
+                printf("%d ",arr[i][j]);
         }
         printf("\n");
     }
 }
-bool cycle_graph(int n,int m,int **arr){
+bool is_cycle_graph(int n,int m,int **arr){
     int s=0;
-    //matrix(n,m,arr);
     for (int i=0;i<n;i++){
         for(int j=0;j<n;j++){
             if (arr[i][j] == 1){
@@ -34,7 +33,7 @@ bool cycle_graph(int n,int m,int **arr){
         return false;
     }
 }
-bool compeleted_graph(int n,int m,int **arr){
+bool is_compeleted_graph(int n,int m,int **arr){
     int d=0;
     for (int i=0;i<n;i++){
         int s=0;
--- main.c
+++ main.c
@@ -2,30 +2,30 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-#include "cycle_and_complete_graphs.c"
+#include "functions.c"
 int main(){
     int n,m;
-    printf("Enter the number of vertices (n): ");
+    //printf("Enter the number of vertices (n): ");
     scanf("%d",&n);
-    printf("Enter the number of edges (m): ");
+    //printf("Enter the number of edges (m): ");
     scanf("%d", &m);
     int** arr = (int**)malloc(n * sizeof(int*));
     for (int i = 0; i < n; i++) {
         arr[i] = (int*)malloc(m * sizeof(int));
     }
-    matrix(n,m,arr);
-    print_matrix(n,m,arr);
-    if (cycle_graph(n,m,arr) == true){
-        printf("Its a cycle graph\n");
+    registre_graph(n,m,arr);
+    print_graph(n,m,arr);
+    if (is_cycle_graph(n,m,arr)){
+        printf("<-----------Its a cycle graph----------->\n");
     }
     else{
-        printf("Its Not a cycle graph\n");
+        printf("<-----------Its Not a cycle graph----------->\n");
     }
-    if (compeleted_graph(n,m,arr) == true){
-        printf("Its a compeleted graph\n");
+    if (is_compeleted_graph(n,m,arr)){
+        printf("<---------Its a compeleted graph--------->\n");
     }
     else{
-        printf("Its Not a completed graph\n");
+        printf("<---------Its Not a completed graph--------->\n");
     }
     return 0;
 }
